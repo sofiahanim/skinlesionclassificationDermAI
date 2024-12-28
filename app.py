6da96e1aca7767dca45d8012fbe234cce27afbaa
hanimsofia <22102228@siswa.um.edu.my>
2024-12-29 07:59:41 +0800

    Update Deployment Codes

--- app.py
+++ app.py
@@ -140,4 +140,5 @@ def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in {'png', 'jpg', 'jpeg'}
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
