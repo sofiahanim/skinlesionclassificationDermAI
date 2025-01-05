5643fd43514ef6c626d258acbd3734730d5f8fa3
hanimsofia <22102228@siswa.um.edu.my>
2025-01-06 07:58:10 +0800

    Update port

--- app.py
+++ app.py
@@ -172,5 +172,5 @@ def serve_image(filename):
 if __name__ == '__main__':
     # Load models before running the app
     load_all_models()
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.getenv('PORT', 5000))
     app.run(debug=True, host='0.0.0.0', port=port)
